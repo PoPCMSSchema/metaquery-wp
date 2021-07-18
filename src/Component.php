73e5032459c8c860bab2ae9c18b5e17a5fac2499
leoloso <leo@getpop.org>
2021-07-18 23:31:51 +0000

    Merge pull request #848 from leoloso/add-meta-modules-to-graphql-api
    Add meta modules to Graphql API

--- src/Component.php
+++ src/Component.php
@@ -19,7 +19,6 @@ class Component extends AbstractComponent
     public static function getDependedComponentClasses(): array
     {
         return [
-            \PoPSchema\MetaQuery\Component::class,
             \PoP\EngineWP\Component::class,
         ];
     }
